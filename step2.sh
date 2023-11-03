351df23e255cb300887557cddc023a947a53f558
ziokim1 <149538556+ziokim1@users.noreply.github.com>
2023-11-03 02:38:17 +0100

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# copy adapter to the current folder
+cp ~/assignment/Trimmomatic-0.39/adapters/TruSeq3-SE.fa .
+
 # Loop through each sample
 for sample in SRR10998570 SRR10998569 SRR10998566 SRR10998565 SRR10998564 SRR10998563 SRR10998562 SRR10998561 SRR10998560 SRR10998559 SRR10998568 SRR10998567
 do
@@ -9,7 +12,7 @@ do
   
   # Run trimmomatic
   java -jar ~/assignment/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads 8 $input1 $output1 \
-  ILLUMINACLIP:~/assignment/Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:10 MINLEN:65
+  ILLUMINACLIP:TruSeq3-SE.fa:2:30:10 HEADCROP:10 MINLEN:65
 done
 
 # run quality check again on the trimmed reads
